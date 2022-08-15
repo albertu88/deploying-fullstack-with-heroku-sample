2fac872c339e1511542b393f06a93f0518641df8
ALBERTO GIMENEZ BEAMONTE <albertu.88@gmail.com>
2022-08-15 11:33:53 +0200

    running heroku

--- services/requests.js
+++ services/requests.js
@@ -2,17 +2,17 @@ const fetch = require('node-fetch'); // import node-fetch (enables the fetch API
 const { Pool } = require('pg'); // import node-postgres
 
 const pool = new Pool({ 
-/*   // create connection to database
+  // create connection to database
    connectionString: process.env.DATABASE_URL,	// use DATABASE_URL environment variable from Heroku app  
     ssl: {
     rejectUnauthorized: false // don't check for SSL cert
-  } */
-      //LOCAL 
+  }
+/*       //LOCAL 
       user: 'postgres',
       host: 'localhost',
       database: 'Trial',
       password: 'postgres',
-      port: 5432
+      port: 5432 */
 
 });
 
